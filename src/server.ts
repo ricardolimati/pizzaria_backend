9bf914b913abf44ff4a015fb617ac790376155a2
Ricardo Lima <ricardodreamsites@gmail.com>
2022-07-15 15:33:04 -0300

    Mudar o listen(3333)

--- src/server.ts
+++ src/server.ts
@@ -30,4 +30,4 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   })
 })
 
-app.listen(3333, () => console.log("Servidor on-line!"));
+app.listen(process.env.PORT || 3333, () => console.log("Server on-line!"));
